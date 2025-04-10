d9c225136418c50c6922850d66bc76aa295bb1d3
Juan Milkah <juanmilkah@gmail.com>
2025-04-10 08:55:30 +0300

    print to screen

--- readme.md
+++ readme.md
@@ -4,4 +4,28 @@ A minimal Operating System. It's definitely a learning experience.
 Skill issues is definitely gonna be playing a huge role in the development
 process. I'm just putting it out there!!
 
+## Build a disk image
+
+```
+cargo bootimage
+```
+
+The bootimage tool performs the following steps behind the scenes:
+
+- It compiles the kernel to an ELF file.
+- It compiles the bootloader dependency as a standalone executable.
+- It links the bytes of the kernel ELF file to the bootloader.
+
+### Booting with Qemu
+
+```bash
+cargo run
+```
+
+Under the hood this is the command being run:
+
+```
+ qemu-system-x86_64 -drive format=raw,file=target/phen_os/debug/bootimage-phren_os.bin
+```
+
 Licensed under the [MIT](LICENSE) license.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Welcome to PhrenOS!")
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static GREETING: &[u8] = b"Welcome to PhrenOs";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in GREETING.iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
